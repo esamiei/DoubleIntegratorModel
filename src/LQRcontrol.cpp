634c4d8a76ec681ceea05d36d8a03787beca922c
ehsamiei <eh.samiei@gmail.com>
2021-11-26 21:55:53 -0800

    LQR modified

--- src/LQRcontrol.cpp
+++ src/LQRcontrol.cpp
@@ -9,16 +9,9 @@ pair<double,Eigen::RowVector2d> LQRControl::LQRControlImp(Mat Q, double R,Vec x)
     Mat Ad=(I+A*DT);
     Vec Bd=B*DT;
 
-    
 
     P=Ad.transpose()*P*Ad-((Ad.transpose()*P*Bd)/(R+Bd.transpose()*P*Bd))*(Bd.transpose()*P*Ad)+Q;
-
-
-
     K=((Ad.transpose()*P*Bd)/(R+Bd.transpose()*P*Bd));
-
-cout<<K;
-
     u_controlled=-K*x;
     return make_pair(u_controlled,K);
 }
